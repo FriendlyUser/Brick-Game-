f135a85a707f7ecb6e6a2b4dfeab5d4670a231dc
David Li <pandabeareee@gmail.com>
2017-10-08 18:07:25 -0700

    Adding the game, will have to update this game later

---
+++ Assets/Scripts/Lose.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose : MonoBehaviour {
+	private Ball ball;
+	private GameManager gameManager;
+
+	IEnumerator Pause() {
+		print("Before Waiting 2 seconds");
+		//Switch GameManager State
+		gameManager = GameObject.FindObjectOfType<GameManager>();
+		gameManager.SwitchState (GameState.Failed);
+		gameManager.ChangeText ("You Lose :(");
+
+		yield return new WaitForSeconds(2);
+
+		//Find the ball and reset game start
+		ball = GameObject.FindObjectOfType<Ball>();
+		ball.gameStarted = false;
+
+		//Reload level
+		Application.LoadLevel(Application.loadedLevel);
+
+		print("After Waiting 2 Seconds");
+	}
+
+	void OnTriggerEnter2D (Collider2D trigger){
+		print ("Lost Triggered!");
+
+		//Wait before restarting level
+		StartCoroutine(Pause());
+
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public int i=0;
+	// Use this for initialization
+	void Start () {
+		//print("This is my first Unity script!");
+	}
+	// Update is called once per frame
+	void Update () {
+		//print(Input.mousePosition);
+		//Set variable for current position
+		Vector3 paddlePos = new Vector3 (8f, this.transform.position.y, 0f);
+		//Get mouse position
+		float mousePos = Input.mousePosition.x / Screen.width * 16;
+		//Set new mouse X position
+		paddlePos.x = Mathf.Clamp(mousePos, 0.75f, 7.25f);
+		//Change paddle to match new X position
+		this.transform.position = paddlePos;
+	}
+}
